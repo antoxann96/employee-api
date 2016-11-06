27aced4d64687528c50eb56cd15ed7a28b20bf95
andrew <andrfas@gmail.com>
2016-11-06 19:34:03 +0200

    added mock test for response with MEDIUM popularity if url sanded  25times

--- test/server/controllers/MocksTest.js
+++ test/server/controllers/MocksTest.js
@@ -29,7 +29,7 @@ describe('twitstat MOCK tests', function () {
         });
     });
 	
-	it('should call the error callback when something went wrong', function(done) {
+    it('should call the error callback when something went wrong', function(done) {
         var expectedEndpoint = 'http://urls.api.twitter.com/1/urls/count.json?url=idmb.com';
         var expectedError = new Error('Not found');
         request.withArgs(expectedEndpoint).yields(expectedError, null, null);
@@ -37,6 +37,24 @@ describe('twitstat MOCK tests', function () {
         twitstat.getPopularity('idmb.com', function(err, data) {
             expect(err).to.equal(expectedError);
             expect(data).to.be.null;
+            done();
+        });
+    });
+
+    it('should response with MEDIUM popularity if url sanded  25times', function (done) {
+        var expectedEndpoint = 'http://urls.api.twitter.com/1/urls/count.json?url=blah.com';
+        var body = JSON.stringify({
+            count: 25,
+            url: "http://blah.com/"
+        });
+        request.withArgs(expectedEndpoint).yields(null, null, body);
+
+        twitstat.getPopularity('blah.com', function (err, data) {
+            expect(err).to.be.null;
+            expect(data).to.equal(JSON.stringify({
+                "url": "http://blah.com/",
+                "popularity": "MEDIUM"
+            }));
             done();
         });
     });
